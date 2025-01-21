e6d882dce7d9dc8e771d25fd1f6d99bb31323858
josielyu <dlyu975@aucklanduni.ac.nz>
2025-01-21 17:26:27 +1300

    fix api issue and implement GetArchivedContactsAsync function

--- Chat.Infra/Migrations/20250121022827_testing.cs
+++ Chat.Infra/Migrations/20250121022827_testing.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Chat.Infra.Migrations
 {
     /// <inheritdoc />
-    public partial class init : Migration
+    public partial class testing : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
