fd42e66943eaa4fb83c15861f9c834c04010709a
josielyu <dlyu975@aucklanduni.ac.nz>
2025-01-25 17:10:59 +1300

    fix bug

--- Chat.Domain/QueryEntities/Contact.cs
+++ Chat.Domain/QueryEntities/Contact.cs
@@ -10,7 +10,7 @@ public sealed class Contact : Entity
         ContactUserId = contactUserId;
         AvatarUrl =
             "https://scontent-syd2-1.xx.fbcdn.net/v/t39.30808-6/461300323_2641222842735125_4681823755008426309_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=8HboOGZaww4Q7kNvgEt4U-_&_nc_zt=23&_nc_ht=scontent-syd2-1.xx&_nc_gid=Ah9eazLO62G-SdnPxUJZliz&oh=00_AYA3h3E0QsQs5xWbfswuoFFCoH60UmPpJCU6rbn02AN2jg&oe=679A2484";
-        lastMessage = "This bug belongs to Josie, all rights reserved.";
+        lastMessage = "我是全公司最帅的男人！";
     }
 
     public Guid UserId { get; private set; }
