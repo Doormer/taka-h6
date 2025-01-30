3aaecb27f1d5e5ae78097c95f8deb60fff32e568
josielyu <dlyu975@aucklanduni.ac.nz>
2025-01-30 16:30:27 +1300

    Add user name

--- Chat.Application/Queries/ChatViewModel.cs
+++ Chat.Application/Queries/ChatViewModel.cs
@@ -16,26 +16,25 @@ public record Contact
         IsRead = false;
 
     }
-    
+
     [DataMember( Name = "id" )]
     public Guid Id { get; init; }
-    
-    [DataMember( Name = "avatarUrl" )]
+
+    [DataMember(Name = "userName")]
     public string UserName { get; init; }
-    
+
     [DataMember( Name = "lastMessageCreatedTime" )]
     public DateTime LastMessageCreatedTime { get; init; }
-    
-    [DataMember( Name = "isArchived" )]
-    
+
+    [DataMember(Name = "isRead")]
     public bool IsRead { get; init; }
-    [DataMember( Name = "isArchived" )]
-    
+
+    [DataMember(Name = "avatarUrl")]
     public string AvatarUrl { get; init; }
-    
+
     [DataMember( Name = "lastMessage" )]
     public string LastMessage { get; init; }
-    
+
     [DataMember( Name = "isArchived" )]
     public bool IsArchived { get; init; }
 }
